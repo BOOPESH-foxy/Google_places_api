ebc0a017f0523e8d55a4880a0a85f9a39f0bb2f0
BOOPESH-foxy <boopesh.mc@gmail.com>
2025-02-05 22:31:05 +0530

    initial commit with python file and trial api - status/working

---
+++ source_details.py
@@ -0,0 +1,36 @@
+import requests
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("GOOGLE_API_KEY")
+PLACE_ID = os.getenv("PLACE_ID")
+BASE_URL = "https://maps.googleapis.com/maps/api/place/details/json"
+
+# Define request parameters
+params = {
+    "place_id": PLACE_ID,
+    "fields": "name,formatted_address,geometry,international_phone_number,rating,opening_hours",
+    "key": API_KEY,
+}
+
+# Make API request
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+
+# Extract relevant information
+if "result" in data:
+    place_info = {
+        "name": data["result"].get("name", ""),
+        "address": data["result"].get("formatted_address", ""),
+        "latitude": data["result"]["geometry"]["location"]["lat"],
+        "longitude": data["result"]["geometry"]["location"]["lng"],
+        "phone": data["result"].get("international_phone_number", ""),
+        "rating": data["result"].get("rating", ""),
+        "opening_hours": str(data["result"].get("opening_hours", {}).get("weekday_text", ""))
+    }
+else:
+    print("Error fetching place details")
+    exit()
